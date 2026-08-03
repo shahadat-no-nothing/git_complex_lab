ffc484de04f08f8bb61122823cbbefd373020b28
shahadat-no-nothing <skhossain.shahadat@gmail.com>
2026-08-03 23:24:39 +0600

    explanation of the microbial community settings

--- app.py
+++ app.py
@@ -2,4 +2,5 @@
 print("Alex: Added global configuration settings")
 print("Lead Dev: Implemented user login feature")
 print(" Alex: Added microbial community settings in partaker")
+# Microbial Community settings gives the user to observe the cell growth individually
 
